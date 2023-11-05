549cc8c5c4bcd4100a274188892910b73108b8db
gonpaul <pavelgoncharov56@gmail.com>
2023-11-05 22:13:11 +0300

    a working site with a gradient background

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import 'tachyons';
+import React from 'react';
+import Cards from "./Cards";
+import SearchBox from './SearchBox.js';
+import { robots } from './robots.js';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super() //we call the parent's constructor method and get access to the parent's properties and methods
+    this.state = {
+      robots: robots,
+      searchfield: ''
+    }
+  }
+
+  onSearchChange = (event) => { // using an arrow function to have an app's context instead of searchbox's one
+    this.setState({ searchfield: event.target.value })
+  }
+
+  render () {
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    })
+    return (
+      <div className="tc">
+        <h1>Robofriends</h1>
+        <SearchBox searchChange={this.onSearchChange}/>
+        <Cards robots={filteredRobots}/>
+      </div>
+    );
+  }
 }
 
 export default App;
